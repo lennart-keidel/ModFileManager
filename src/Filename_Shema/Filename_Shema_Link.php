f002bbf074c9b713b58e85b889293a9d852eb312
lennart-keidel <privat@lennart-keidel.de>
2024-01-07 17:20:30 +0100

    src/Filename_Shema/Filename_Shema_Link.php - changed search input for link type from url to text

--- src/Filename_Shema/Filename_Shema_Link.php
+++ src/Filename_Shema/Filename_Shema_Link.php
@@ -27,7 +27,7 @@ abstract class Filename_Shema_Link extends Compareable_Text_Operand implements I
       <select class="%3$s_operand%1$d %3$s%1$d" id="%3$s_operand%1$d" name="%2$s[%1$d]['.Ui::ui_search_data_key_operand_root.']['.self::class.'][]">
         %4$s
       </select>
-      <input class="%3$s_value%1$d %3$s%1$d" id="'.self::class.'%1$d" type="url" name="%2$s[%1$d]['.Ui::ui_search_data_key_value_root.']['.self::class.'][]" autocomplete="off">
+      <input class="%3$s_value%1$d %3$s%1$d" id="'.self::class.'%1$d" type="text" name="%2$s[%1$d]['.Ui::ui_search_data_key_value_root.']['.self::class.'][]" autocomplete="off">
       %5$s
     </div>
   ';

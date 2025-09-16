d12028de3ecb444aa02aebe6c1039cd0570e363a
lennart-keidel <32912465+lennart-keidel@users.noreply.github.com>
2025-09-16 22:59:10 +0200

    Update start_xampp_server_helper.sh

--- start_xampp_server_helper.sh
+++ start_xampp_server_helper.sh
@@ -1,16 +1,8 @@
-path_xampp_root_directory="/c/xampp"
-path_start_xampp_executeable="$path_xampp_root_directory/xampp_start.exe"
-path_stop_xampp_executeable="$path_xampp_root_directory/xampp_stop.exe"
+# variables
 custom_localhost_domain='mod-dateinamenmanager.local'
-
-# error if xampp root directory not existing
-if ! [ -f $path_start_xampp_executeable ]; then
-  echo -e "FEHLER: XAMPP ist nicht installiert oder konnte nicht gefunden werden."
-  echo -e "Fehlender Pfad: $path_xampp_root_directory"
-fi
-
-# start xampp
-$path_start_xampp_executeable
 
 # open web page with app
 start "http://$custom_localhost_domain"
+
+# start php internal webserver
+php -S $custom_localhost_domain:80

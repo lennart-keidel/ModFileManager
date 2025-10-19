c37aafeb34ddf014ed07604d15c7b6b890eef463
lennart-keidel <32912465+lennart-keidel@users.noreply.github.com>
2025-10-19 20:20:41 +0200

    src/Filename_Shema/Sub_Data_Categorie_CCBUY_Categorie.php - moved the order of the categories - formated code

--- src/Filename_Shema/Sub_Data_Categorie_CCBUY_Categorie.php
+++ src/Filename_Shema/Sub_Data_Categorie_CCBUY_Categorie.php
@@ -2,7 +2,8 @@
 
 # this class is only required for a clean structuring of the ui and filename data
 # it's only for ment to be a sub class of Filename_Shema_Flag
-abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand implements I_Filename_Shema {
+abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand implements I_Filename_Shema
+{
 
   public const array_ui_data_key = [
     self::class
@@ -111,18 +112,38 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
   private const input_shema_template = '
   <div class="container_label_and_input sub_input option_cc_buy_sub_data_categorie%1$d">
     <label for="%3$s%1$d">Buy-Kategorie</label>
-    <select class="%3$s%1$d option_cc_buy_sub_data_categorie%1$d" name="%2$s[%1$d]['.self::class.']" id="%3$s%1$d" %4$s>
+    <select class="%3$s%1$d option_cc_buy_sub_data_categorie%1$d" name="%2$s[%1$d][' . self::class . ']" id="%3$s%1$d" %4$s>
       <option value="" selected disabled>Auswählen</option>
+      <optgroup label="Deko">
+        <option value="option_deco_plant">Pflanze</option>
+        <option value="option_deco_picture">Bild oder Poster</option>
+        <option value="option_deco_mirror">Spiegel</option>
+        <option value="option_deco_curtain">Gardiene</option>
+        <option value="option_deco_rug">Teppich</option>
+        <option value="option_deco_roof">Dachdekoration</option>
+        <option value="option_deco_sculpture">Skulptur</option>
+        <option value="option_deco_misc">Verschiedenes</option>
+      </optgroup>
+      <optgroup label="Komfort">
+        <option value="option_comfort_bed">Bett</option>
+        <option value="option_comfort_dining_chair">Esszimmerstuhl</option>
+        <option value="option_comfort_armchair">Sessel</option>
+        <option value="option_comfort_sofa">Sofa</option>
+        <option value="option_comfort_lounge_chair">Liege</option>
+        <option value="option_comfort_misc">Verschiedenes</option>
+      </optgroup>
+      <optgroup label="Beleuchtung">
+        <option value="option_lighting_table">Tischlampe</option>
+        <option value="option_lighting_stand">Stehlampe</option>
+        <option value="option_lighting_wall">Wandlampe</option>
+        <option value="option_lighting_ceiling">Deckenlampe</option>
+        <option value="option_lighting_outdoor">Außenbeleuchtung</option>
+      </optgroup>
       <optgroup label="Sanitär">
         <option value="option_plumbing_sink">Waschbecken</option>
         <option value="option_plumbing_toilet">Toilette</option>
         <option value="option_plumbing_shower_tub">Dusche oder Wanne</option>
         <option value="option_plumbing_misc">Verschiedenes</option>
-      </optgroup>
-      <optgroup label="Geräte">
-        <option value="option_appliances_large">Große Geräte</option>
-        <option value="option_appliances_small">kleine Geräte</option>
-        <option value="option_appliances_misc">Verschiedenes</option>
       </optgroup>
       <optgroup label="Oberflächen">
         <option value="option_surfaces_counter">Theke</option>
@@ -134,13 +155,15 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
         <option value="option_surfaces_display">Schaukästen</option>
         <option value="option_surfaces_misc">Verschiedenes</option>
       </optgroup>
-      <optgroup label="Komfort">
-        <option value="option_comfort_bed">Bett</option>
-        <option value="option_comfort_dining_chair">Esszimmerstuhl</option>
-        <option value="option_comfort_armchair">Sessel</option>
-        <option value="option_comfort_sofa">Sofa</option>
-        <option value="option_comfort_lounge_chair">Liege</option>
-        <option value="option_comfort_misc">Verschiedenes</option>
+      <optgroup label="Geräte">
+        <option value="option_appliances_large">Große Geräte</option>
+        <option value="option_appliances_small">kleine Geräte</option>
+        <option value="option_appliances_misc">Verschiedenes</option>
+      </optgroup>
+      <optgroup label="Schränke">
+        <option value="option_storage_bookshelf">Bücherregal</option>
+        <option value="option_storage_drawer">Kommode</option>
+        <option value="option_storage_misc">Verschiedenes</option>
       </optgroup>
       <optgroup label="Elektronik">
         <option value="option_electronics_tv">Fernseher</option>
@@ -153,28 +176,6 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
         <option value="option_entertainment_hobbys_and_skills">Hobbys und Fähigkeiten</option>
         <option value="option_entertainment_party">Party</option>
         <option value="option_entertainment_misc">Verschiedenes</option>
-      </optgroup>
-      <optgroup label="Beleuchtung">
-        <option value="option_lighting_table">Tischlampe</option>
-        <option value="option_lighting_stand">Stehlampe</option>
-        <option value="option_lighting_wall">Wandlampe</option>
-        <option value="option_lighting_ceiling">Deckenlampe</option>
-        <option value="option_lighting_outdoor">Außenbeleuchtung</option>
-      </optgroup>
-      <optgroup label="Deko">
-        <option value="option_deco_plant">Pflanze</option>
-        <option value="option_deco_picture">Bild oder Poster</option>
-        <option value="option_deco_mirror">Spiegel</option>
-        <option value="option_deco_curtain">Gardiene</option>
-        <option value="option_deco_rug">Teppich</option>
-        <option value="option_deco_roof">Dachdekoration</option>
-        <option value="option_deco_sculpture">Skulptur</option>
-        <option value="option_deco_misc">Verschiedenes</option>
-      </optgroup>
-      <optgroup label="Schränke">
-        <option value="option_storage_bookshelf">Bücherregal</option>
-        <option value="option_storage_drawer">Kommode</option>
-        <option value="option_storage_misc">Verschiedenes</option>
       </optgroup>
       <optgroup label="Kinder">
         <option value="option_childreen_furniture">Kinder Möbel</option>
@@ -210,7 +211,7 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
     <script>
       document.addEventListener("DOMContentLoaded", function(){
         setTimeout(function(){
-          disable_and_hide_input_by_class_name_if_source_element_is_not_selected(\''.Filename_Shema_Categorie::class.'%1$d\', \'option_cc_buy\', \'option_cc_buy_sub_data_categorie%1$d\');
+          disable_and_hide_input_by_class_name_if_source_element_is_not_selected(\'' . Filename_Shema_Categorie::class . '%1$d\', \'option_cc_buy\', \'option_cc_buy_sub_data_categorie%1$d\');
         },100);
       });
     </script>
@@ -222,21 +223,41 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
   private const search_input_shema_template = '
   <div class="container_label_and_input sub_input option_cc_buy_sub_data_categorie%1$d">
     <label for="%3$s%1$d">Buy-Kategorie</label>
-    <select class="%3$s_operand%1$d %3$s%1$d" name="%2$s[%1$d]['.Ui::ui_search_data_key_operand_root.']['.self::class.'][]">
+    <select class="%3$s_operand%1$d %3$s%1$d" name="%2$s[%1$d][' . Ui::ui_search_data_key_operand_root . '][' . self::class . '][]">
       %4$s
     </select>
-    <select class="%3$s%1$d option_cc_buy_sub_data_categorie%1$d" name="%2$s[%1$d]['.Ui::ui_search_data_key_value_root.']['.self::class.'][]" id="%3$s%1$d" required>
+    <select class="%3$s%1$d option_cc_buy_sub_data_categorie%1$d" name="%2$s[%1$d][' . Ui::ui_search_data_key_value_root . '][' . self::class . '][]" id="%3$s%1$d" required>
       <option value="" selected disabled>Auswählen</option>
+      <optgroup label="Deko">
+        <option value="option_deco_plant">Pflanze</option>
+        <option value="option_deco_picture">Bild oder Poster</option>
+        <option value="option_deco_mirror">Spiegel</option>
+        <option value="option_deco_curtain">Gardiene</option>
+        <option value="option_deco_rug">Teppich</option>
+        <option value="option_deco_roof">Dachdekoration</option>
+        <option value="option_deco_sculpture">Skulptur</option>
+        <option value="option_deco_misc">Verschiedenes</option>
+      </optgroup>
+      <optgroup label="Komfort">
+        <option value="option_comfort_bed">Bett</option>
+        <option value="option_comfort_dining_chair">Esszimmerstuhl</option>
+        <option value="option_comfort_armchair">Sessel</option>
+        <option value="option_comfort_sofa">Sofa</option>
+        <option value="option_comfort_lounge_chair">Liege</option>
+        <option value="option_comfort_misc">Verschiedenes</option>
+      </optgroup>
+      <optgroup label="Beleuchtung">
+        <option value="option_lighting_table">Tischlampe</option>
+        <option value="option_lighting_stand">Stehlampe</option>
+        <option value="option_lighting_wall">Wandlampe</option>
+        <option value="option_lighting_ceiling">Deckenlampe</option>
+        <option value="option_lighting_outdoor">Außenbeleuchtung</option>
+      </optgroup>
       <optgroup label="Sanitär">
         <option value="option_plumbing_sink">Waschbecken</option>
         <option value="option_plumbing_toilet">Toilette</option>
         <option value="option_plumbing_shower_tub">Dusche oder Wanne</option>
         <option value="option_plumbing_misc">Verschiedenes</option>
-      </optgroup>
-      <optgroup label="Geräte">
-        <option value="option_appliances_large">Große Geräte</option>
-        <option value="option_appliances_small">kleine Geräte</option>
-        <option value="option_appliances_misc">Verschiedenes</option>
       </optgroup>
       <optgroup label="Oberflächen">
         <option value="option_surfaces_counter">Theke</option>
@@ -248,13 +269,15 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
         <option value="option_surfaces_display">Schaukästen</option>
         <option value="option_surfaces_misc">Verschiedenes</option>
       </optgroup>
-      <optgroup label="Komfort">
-        <option value="option_comfort_bed">Bett</option>
-        <option value="option_comfort_dining_chair">Esszimmerstuhl</option>
-        <option value="option_comfort_armchair">Sessel</option>
-        <option value="option_comfort_sofa">Sofa</option>
-        <option value="option_comfort_lounge_chair">Liege</option>
-        <option value="option_comfort_misc">Verschiedenes</option>
+      <optgroup label="Geräte">
+        <option value="option_appliances_large">Große Geräte</option>
+        <option value="option_appliances_small">kleine Geräte</option>
+        <option value="option_appliances_misc">Verschiedenes</option>
+      </optgroup>
+      <optgroup label="Schränke">
+        <option value="option_storage_bookshelf">Bücherregal</option>
+        <option value="option_storage_drawer">Kommode</option>
+        <option value="option_storage_misc">Verschiedenes</option>
       </optgroup>
       <optgroup label="Elektronik">
         <option value="option_electronics_tv">Fernseher</option>
@@ -267,28 +290,6 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
         <option value="option_entertainment_hobbys_and_skills">Hobbys und Fähigkeiten</option>
         <option value="option_entertainment_party">Party</option>
         <option value="option_entertainment_misc">Verschiedenes</option>
-      </optgroup>
-      <optgroup label="Beleuchtung">
-        <option value="option_lighting_table">Tischlampe</option>
-        <option value="option_lighting_stand">Stehlampe</option>
-        <option value="option_lighting_wall">Wandlampe</option>
-        <option value="option_lighting_ceiling">Deckenlampe</option>
-        <option value="option_lighting_outdoor">Außenbeleuchtung</option>
-      </optgroup>
-      <optgroup label="Deko">
-        <option value="option_deco_plant">Pflanze</option>
-        <option value="option_deco_picture">Bild oder Poster</option>
-        <option value="option_deco_mirror">Spiegel</option>
-        <option value="option_deco_curtain">Gardiene</option>
-        <option value="option_deco_rug">Teppich</option>
-        <option value="option_deco_roof">Dachdekoration</option>
-        <option value="option_deco_sculpture">Skulptur</option>
-        <option value="option_deco_misc">Verschiedenes</option>
-      </optgroup>
-      <optgroup label="Schränke">
-        <option value="option_storage_bookshelf">Bücherregal</option>
-        <option value="option_storage_drawer">Kommode</option>
-        <option value="option_storage_misc">Verschiedenes</option>
       </optgroup>
       <optgroup label="Kinder">
         <option value="option_childreen_furniture">Kinder Möbel</option>
@@ -324,7 +325,7 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
     <script>
       document.addEventListener("DOMContentLoaded", function(){
         setTimeout(function(){
-          disable_and_hide_input_by_class_name_if_source_element_is_not_selected(\''.Filename_Shema_Categorie::class.'%1$d\', \'option_cc_buy\', \'option_cc_buy_sub_data_categorie%1$d\');
+          disable_and_hide_input_by_class_name_if_source_element_is_not_selected(\'' . Filename_Shema_Categorie::class . '%1$d\', \'option_cc_buy\', \'option_cc_buy_sub_data_categorie%1$d\');
         },100);
       });
     </script>
@@ -333,27 +334,31 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
 
 
   # if conditions are met, manipulate something in the input data
-  public static function manipulate_ui_data(array $data_from_ui) : array {
+  public static function manipulate_ui_data(array $data_from_ui): array
+  {
     return $data_from_ui;
   }
 
 
   # print filename shema input to ui
-  public static function print_filename_shema_input_for_ui(int $index, string $different_ui_key_root = null, bool $is_required = true) : void {
+  public static function print_filename_shema_input_for_ui(int $index, string $different_ui_key_root = null, bool $is_required = true): void
+  {
     printf(self::input_shema_template, $index, ($different_ui_key_root === null ? Ui::ui_data_key_root : $different_ui_key_root), self::class, ($is_required === true ? "required" : ""));
   }
 
 
   # generate filename shema input to ui
   # return html string
-  public static function generate_filename_shema_input_for_ui(int $index, string $different_ui_key_root = null, bool $is_required = true) : string {
+  public static function generate_filename_shema_input_for_ui(int $index, string $different_ui_key_root = null, bool $is_required = true): string
+  {
     return sprintf(self::input_shema_template, $index, ($different_ui_key_root === null ? Ui::ui_data_key_root : $different_ui_key_root), self::class, ($is_required === true ? "required" : ""));
   }
 
 
 
   # print filename shema search input to ui
-  public static function print_filename_shema_search_input_for_ui(int $index) : void {
+  public static function print_filename_shema_search_input_for_ui(int $index): void
+  {
     $operand_select_option_html = Ui::generate_search_operand_select_options_ui(self::class);
     printf(self::search_input_shema_template, $index, Ui::ui_search_data_key_root, self::class, $operand_select_option_html);
   }
@@ -361,14 +366,16 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
 
   # generate filename shema search input to ui
   # return html string
-  public static function generate_filename_shema_search_input_for_ui(int $index) : string {
+  public static function generate_filename_shema_search_input_for_ui(int $index): string
+  {
     $operand_select_option_html = Ui::generate_search_operand_select_options_ui(self::class);
     return sprintf(self::search_input_shema_template, $index, Ui::ui_search_data_key_root, self::class, $operand_select_option_html);
   }
 
 
   # get target path considering the conditions of this shema input
-  public static function get_target_path_by_condition(array $data_for_one_filename, string $source_path) : array {
+  public static function get_target_path_by_condition(array $data_for_one_filename, string $source_path): array
+  {
     $path_result = "";
     $success_heading = "";
     $error_heading = "";
@@ -377,12 +384,13 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
 
 
   # convert data collected from ui to usable data for following process
-  public static function convert_ui_data_to_data(array $data_from_ui) : array {
+  public static function convert_ui_data_to_data(array $data_from_ui): array
+  {
 
     # filter data for this schema from whole ui data
     $key = current(self::array_ui_data_key);
 
-    if(!isset($data_from_ui[$key])){
+    if (!isset($data_from_ui[$key])) {
       throw new Shema_Exception("Fehler bei Verarbeitung der Daten.\\nFehlender Schlüssel in POST-Request: '$key'");
     }
 
@@ -393,29 +401,30 @@ abstract class Sub_Data_Categorie_CCBUY_Categorie extends Compareable_Is_Operand
 
 
   # convert data to filename part using this shema
-  public static function convert_data_to_filename(array $data_converted) : string{
+  public static function convert_data_to_filename(array $data_converted): string
+  {
     $key = current($data_converted);
-    if(isset(self::array_option_id[$key]) === false){
-      throw new Shema_Exception("Fehler bei Verarbeitung der Daten.\\nFehlender Wert: ".$key);
+    if (isset(self::array_option_id[$key]) === false) {
+      throw new Shema_Exception("Fehler bei Verarbeitung der Daten.\\nFehlender Wert: " . $key);
     }
     return self::array_option_id[$key];
   }
 
 
   # reverse process: converting filename back to data
-  public static function convert_filename_to_data(string $filename_part) : array{
-    if(empty($filename_part)){
+  public static function convert_filename_to_data(string $filename_part): array
+  {
+    if (empty($filename_part)) {
       throw new Shema_Exception("Fehler bei Verarbeitung der Daten. Lehren Dateinamen erhalten.");
     }
 
     # search for short id and get key
     $key = array_search($filename_part, self::array_option_id);
-    if($key === false){
+    if ($key === false) {
       throw new Shema_Exception("Fehler bei Verarbeitung der Daten.\\nDer Wert '$filename_part' ist für die Kategorie nicht valide.");
     }
 
     # return array in format of original ui data
     return [current(self::array_ui_data_key) => $key];
   }
-
 }
